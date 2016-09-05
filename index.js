10ef8183492d6ae70ffa93bcae3710cccbffde21
TerraZero <p.sagitza@loom-berlin.com>
2016-09-05 10:37:05 +0200

    change connect to express

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-const connect = require('connect');
+const express = require('express');
 const serveStatic = require('serve-static');
 
 // create server
-const connection = connect();
+const app = express();
 const serving = serveStatic(__dirname + '/web', {fallthrough: false});
 
 function redirect(response, location, logging = '|- Redirect') {
@@ -13,24 +13,24 @@ function redirect(response, location, logging = '|- Redirect') {
   response.end();
 }
 
-connection.use(function serveLog(request, response, next) {
+app.use(function serveLog(request, response, next) {
   console.log('Serve: ' + request.url);
   next();
 });
 
-// connection.use('/test', function dynamicServe(request, response, next) {
+// app.use('/test', function dynamicServe(request, response, next) {
 //   console.log('|- Dynamic: ' + request.url);
 
 //   response.setHeader('Content-Type', 'text/plain');
 //   response.end('Dynamic root: ' + request.url);
 // });
 
-connection.use(function serve(request, response, next) {
+app.use(function serve(request, response, next) {
   console.log('|- Static: ' + request.url);
   serving(request, response, next);
 });
 
-connection.use(function redirectHtmlFile(err, request, response, next) {
+app.use(function redirectHtmlFile(err, request, response, next) {
   console.log('|--- Static: Url is not static!');
 
   if (err.statusCode == 404 && !request.url.endsWith('.html')) {
@@ -40,7 +40,7 @@ connection.use(function redirectHtmlFile(err, request, response, next) {
   }
 });
 
-connection.use(function notFoundHandler(err, request, response, next) {
+app.use(function notFoundHandler(err, request, response, next) {
   if (err.statusCode == 404) {
     redirect(response, '/error/404.html');
   } else {
@@ -48,12 +48,12 @@ connection.use(function notFoundHandler(err, request, response, next) {
   }
 });
 
-connection.use(function errorHandler(err, request, response, next) {
+app.use(function errorHandler(err, request, response, next) {
   console.error('|- Error (' + err.statusCode + '): ' + err);
 });
 
 // start server
-connection.listen(8080, function() {
+app.listen(8080, function() {
   console.log();
   console.log('Server is available on: ');
   console.log('localhost:8080');
